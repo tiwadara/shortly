919c0cebb622b9957a77b5b12d90fdfb613d4838
Tiwalola <tiwababalola@gmail.com>
2021-07-19 19:02:06 +0100

    fix: hide keyboard on submit

--- shortlyapp/app/src/main/java/com/tiwa/shortlyapp/ui/home/HomeActivity.kt
+++ shortlyapp/app/src/main/java/com/tiwa/shortlyapp/ui/home/HomeActivity.kt
@@ -2,9 +2,11 @@ package com.tiwa.shortlyapp.ui.home
 
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.RESULT_UNCHANGED_SHOWN
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -39,15 +41,16 @@ class HomeActivity : AppCompatActivity() {
         initializeAdapter()
         edit_text_short_link.setOnFocusChangeListener { _, _ ->
             edit_text_short_link.hint = getString(R.string.msg_shorten_a_link_here)
-            edit_text_short_link.setHintTextColor(ResourcesCompat.getColor(resources,R.color.text_color,null))
-            edit_text_short_link.background = ContextCompat.getDrawable(this,R.drawable.bg_edit_text)
+            edit_text_short_link.setHintTextColor(ResourcesCompat.getColor(resources, R.color.text_color, null))
+            edit_text_short_link.background = ContextCompat.getDrawable(this, R.drawable.bg_edit_text)
         }
         button_shorten.setOnClickListener {
+            hideKeyboard()
             if (edit_text_short_link.text.isNullOrEmpty()) {
                 edit_text_short_link.hint = getString(R.string.msg_please_add_a_link_here)
-                edit_text_short_link.setHintTextColor(ResourcesCompat.getColor(resources,R.color.error,null))
-                edit_text_short_link.background = ContextCompat.getDrawable(this,R.drawable.bg_edit_text_error)
-            }else {
+                edit_text_short_link.setHintTextColor(ResourcesCompat.getColor(resources, R.color.error, null))
+                edit_text_short_link.background = ContextCompat.getDrawable(this, R.drawable.bg_edit_text_error)
+            } else {
                 viewModel.createShortLink(edit_text_short_link.text.toString())
             }
         }
@@ -56,19 +59,19 @@ class HomeActivity : AppCompatActivity() {
     private fun initializeAdapter() {
         recycler_view.layoutManager = LinearLayoutManager(this)
         adapter = HistoryAdapter(
-            shortLinks,
-            { position, id ->
-                viewModel.deleteShortLink(id)
-                shortLinks.removeAt(position)
-                adapter.notifyItemRemoved(position)
-                adapter.notifyItemRangeChanged(position, shortLinks.size)
-            }, { url ->
-                val clipboard = ContextCompat.getSystemService(this, ClipboardManager::class.java)
-                val clip = ClipData.newPlainText(getString(R.string.title_shortened_url), url)
-                clipboard?.setPrimaryClip(clip)
-                toastMessage(getString(R.string.msg_url_copied))
+                shortLinks,
+                { position, id ->
+                    viewModel.deleteShortLink(id)
+                    shortLinks.removeAt(position)
+                    adapter.notifyItemRemoved(position)
+                    adapter.notifyItemRangeChanged(position, shortLinks.size)
+                }, { url ->
+            val clipboard = ContextCompat.getSystemService(this, ClipboardManager::class.java)
+            val clip = ClipData.newPlainText(getString(R.string.title_shortened_url), url)
+            clipboard?.setPrimaryClip(clip)
+            toastMessage(getString(R.string.msg_url_copied))
 
-            }, this)
+        }, this)
         recycler_view.adapter = adapter
     }
 
@@ -118,6 +121,11 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun toggleLoader(show: Boolean) {
-        if (show) progress.visibility = View.VISIBLE else  progress.visibility = View.GONE
+        if (show) progress.visibility = View.VISIBLE else progress.visibility = View.GONE
+    }
+
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+        imm?.hideSoftInputFromWindow(layout_bottom_view.applicationWindowToken, RESULT_UNCHANGED_SHOWN)
     }
 }
